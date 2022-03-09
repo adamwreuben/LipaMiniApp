d9b627163ac6f8ebc1cc5611c5849e580c2467cd
adamwreuben <adamreuben66@gmail.com>
2022-03-09 17:26:26 +0300

    first commit

---
+++ src/app/app.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import { Button } from 'antd-mobile';
+
+function App () {
+    return (
+        <>
+            <h3 align="center">Welcome to Lipa</h3>
+            <div align="center" >
+                <Button color="success">Get started!</Button>
+            </div>
+        </>
+    );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ESLintPlugin from 'eslint-webpack-plugin';
+
+var distFolder = process.cwd()+"/dist";
+
+const options = {
+  extensions: [`js`, `jsx`,`cjs`],
+  exclude: [
+    `/node_modules/`,
+    `/bower_components/`
+  ]
+}
+
+export default {
+    entry: './src/index.js',
+    output: {
+      path: distFolder,
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 10240,
+              },
+            },
+          ]
+        },
+      {
+        test: /\.(js|jsx|cjs)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader',
+        ],
+      },
+      ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: path.resolve('./index.html'),
+        }),
+        new ESLintPlugin(options)
+    ]
+};
