92ce402296e2780d6ac862918a2d739558844372
adamwreuben <adamreuben66@gmail.com>
2022-03-10 17:07:38 +0300

    add compression with brotlicompress

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 
 import path from 'path';
+import zlib from 'zlib';
+
+import CompressionPlugin from 'compression-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ESLintPlugin from 'eslint-webpack-plugin';
 
@@ -14,6 +17,7 @@ const options = {
 }
 
 export default {
+    mode: 'production',
     entry: './src/index.js',
     output: {
       path: distFolder,
@@ -54,6 +58,30 @@ export default {
         new HtmlWebpackPlugin({
           template: path.resolve('./index.html'),
         }),
-        new ESLintPlugin(options)
+        new ESLintPlugin(options),
+        new CompressionPlugin({
+          filename: "[path][base].br",
+          algorithm: "brotliCompress",
+          test: /\.(js|css|html|svg)$/,
+          compressionOptions: {
+            params: {
+              [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+            },
+          },
+          threshold: 10240,
+          minRatio: 0.8,
+          deleteOriginalAssets: true,
+        }),
+
+        new CompressionPlugin({
+          deleteOriginalAssets: true,
+          test: /\.(png|jpg|gif|woff|woff2|eot|ttf)$/,
+          compressionOptions: {
+            numiterations: 15,
+          },
+          algorithm(input, compressionOptions, callback) {
+            return zopfli.gzip(input, compressionOptions, callback);
+          },
+        })
     ]
 };
